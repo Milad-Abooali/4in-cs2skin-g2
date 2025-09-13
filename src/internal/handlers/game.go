27643fe8305641404799b418cea94abbf04f693f
Milad Abooali <m.abooali@hotmail.com>
2025-09-13 06:45:18 +0330

    feat: Live Game

--- src/internal/handlers/game.go
+++ src/internal/handlers/game.go
@@ -103,7 +103,13 @@ func NextGame(id int64) {
 
 func startGameLoop(game models.Game) {
 	go func() {
-		speed := 10000 / game.CrashAt
+		speed := 10000
+		if game.CrashAt < 5 {
+			speed = 500
+		}
+		if game.CrashAt > 5 {
+			speed = int(10000 / game.CrashAt)
+		}
 		if speed < 150 {
 			speed += 150
 		}
