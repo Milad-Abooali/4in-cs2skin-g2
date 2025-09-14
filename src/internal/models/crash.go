6fbb235a150d7760f350b637f8645382fc8f4b20
Milad Abooali <m.abooali@hotmail.com>
2025-09-14 10:11:53 +0330

    feat: Live Game

--- src/internal/models/crash.go
+++ src/internal/models/crash.go
@@ -13,21 +13,24 @@ type LiveGame struct {
 type Bet struct {
 	ID         int64     `json:"id"`
 	UserID     int64     `json:"userID"`
+	Username   string    `json:"username"`
+	Avatar     string    `json:"avatar"`
+	XPLevel    int       `json:"xpLevel"`
 	GameID     int64     `json:"gameID"`
 	Bet        float64   `json:"bet"`
 	Payout     float64   `json:"payout"`
 	Multiplier float64   `json:"multiplier"`
 	CheckoutOn float64   `json:"checkoutOn"`
-	CheckoutBy string    `json:"CheckoutBy"`
+	CheckoutBy string    `json:"checkoutBy"`
 	CreatedAt  time.Time `json:"createdAt"`
 }
 
 type Game struct {
 	ID             int64     `json:"id"`
-	StartAt        time.Time `json:"StartAt"`
-	EndAt          time.Time `json:"EndAt"`
-	Multiplier     float64   `json:"Multiplier"`
-	CrashAt        float64   `json:"CrashAt"`
+	StartAt        time.Time `json:"startAt"`
+	EndAt          time.Time `json:"endAt"`
+	Multiplier     float64   `json:"multiplier"`
+	CrashAt        float64   `json:"crashAt"`
 	ServerSeedHash string    `json:"serverSeedHash"`
 	ServerSeed     string    `json:"serverSeed"`
 }
