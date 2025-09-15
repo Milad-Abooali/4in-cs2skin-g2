811af6448363552c62ad7d86b9be658d86c1741c
Milad Abooali <m.abooali@hotmail.com>
2025-09-15 08:34:25 +0330

    feat: Live Game

--- src/internal/handlers/ping.go
+++ src/internal/handlers/ping.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"github.com/Milad-Abooali/4in-cs2skin-g2/src/internal/events"
 	"github.com/Milad-Abooali/4in-cs2skin-g2/src/internal/models"
 	"time"
 )
@@ -11,6 +12,11 @@ func Ping(data map[string]interface{}) (models.HandlerOK, models.HandlerError) {
 		errR models.HandlerError
 		resR models.HandlerOK
 	)
+
+	events.Emit("all", "history", History.GetAll())
+	events.Emit("all", "leaderboard", Leaderboard.GetAll())
+	events.Emit("all", "liveBets", LiveBets)
+	events.Emit("all", "liveGame", LiveGame)
 
 	// Success
 	resR.Type = "ping"
