77735281f171c6107a7818f384e8b20a8e7511e8
Milad Abooali <m.abooali@hotmail.com>
2025-09-17 12:05:15 +0330

    feat: Live Game

--- src/apiapp/models.go
+++ src/apiapp/models.go
@@ -5,7 +5,7 @@ var apiURL = "https://api.main.cs2skin.com"
 type Payload struct {
 	GID         int    `json:"g_id"`
 	GTime       string `json:"g_time"`
-	DisplayName string `json:"displayName"`
+	DisplayName string `json:"username"`
 	Bet         string `json:"bet"`
 	Multiplier  string `json:"multiplier"`
 	Payout      string `json:"payout"`
