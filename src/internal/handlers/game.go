6fbbe48a8e20691b13ac4f0386851f910a107d3c
Milad Abooali <m.abooali@hotmail.com>
2025-09-27 03:31:00 +0330

    feat: Live Game

--- src/internal/handlers/game.go
+++ src/internal/handlers/game.go
@@ -220,7 +220,7 @@ func endGame(game models.Game) {
 	LiveGame.GameState = StateFinished
 	// events.Emit("all", "liveGame", LiveGame)
 
-	LiveGame.Tracker.Save("g2_games", int(LiveGame.ID))
+	LiveGame.Tracker.Save("g2_games", int(game.ID))
 
 	// time.Sleep(1000 * time.Millisecond)
 	log.Printf("Game %d Ended", game.ID)
